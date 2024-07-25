42d1dc3408bae0036a5bf895f569c105bc5d4e8a
ALEKSANDRBLOKHIN <148769746+ALEKSANDRBLOKHIN@users.noreply.github.com>
2024-07-25 13:28:06 +0200

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -6,12 +6,10 @@ from config import  db_config
 def get_items_from_db(price=None, rooms=None, place=None, index=0, active=None):
     connection = mysql.connector.connect(**db_config)
     cursor = connection.cursor(dictionary=True)
-    # Начало запроса
     query = "SELECT * FROM rent1"
     conditions = []
     params = []
 
-    # Проверка условий и добавление их к запросу
     if price is not None:
         conditions.append("price <= %s")
         params.append(price)
@@ -25,15 +23,14 @@ def get_items_from_db(price=None, rooms=None, place=None, index=0, active=None):
         conditions.append("active = %s")
         params.append(active)
 
-    # Если есть условия, добавляем их к запросу
     if conditions:
         query += " WHERE " + " AND ".join(conditions)
 
-    # Добавляем лимит и смещение
+
     query += " LIMIT 1 OFFSET %s"
     params.append(index)
 
-    # Выполнение запроса
+    
     cursor.execute(query, params)
     items = cursor.fetchall()
 
